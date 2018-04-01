76f459ff3c4ed9a6dc400ec9ce7cd6c4707800d9
sebastian.mihalache <sasha.mihalache@gmail.com>
2018-04-01 17:57:14 +0300

    Spinner component + fix burger menu

--- app/src/layout/Navbar/Navbar.js
+++ app/src/layout/Navbar/Navbar.js
@@ -6,7 +6,9 @@ class Navbar extends React.Component {
     return (
       <nav>
         <div className="container">
-          <div className="menu" onClick={this.props.onToggleOverlay} />
+          <div className="menu">
+            <div className="burger" onClick={this.props.onToggleOverlay} />
+          </div>
           <div className="title">Designer's Mix</div>
           <div className="action-panel">
             <div className="login">Login</div>
--- app/src/views/MixesView/MixesView.js
+++ app/src/views/MixesView/MixesView.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./MixesView.scss";
 import { getData } from "../../utils/CardsRepository";
 import Card from '../../components/Card/Card';
+import Spinner from '../../components/Spinner/Spinner';
 
 class CardsView extends React.Component {
   state = {
@@ -10,22 +11,36 @@ class CardsView extends React.Component {
 
   componentDidMount() {
     getData().then(cards => {
-      this.setState({ cards });
+      setTimeout(() => {
+          this.setState({ cards });
+      }, 1000);
     });
   }
+
+  renderMixes = () => (
+    <div>
+      <div className="mixes-filter">
+          <div className="filter active">Recent</div>
+          <div className="filter">Popular</div>
+        </div>
+      <div className="mixes-list">
+        {this.state.cards.map(card => {
+          return <Card card={card} key={card.id} />;
+        })}
+      </div>
+    </div>
+  )
 
   render() {
     return (
       <div className="mixes-view">
-        <div className="mixes-filter">
-          <div className="filter active">Recent</div>
-          <div className="filter">Popular</div>
-        </div>
-        <div className="mixes-list">
-          {this.state.cards.map(card => {
-            return <Card card={card} key={card.id} />;
-          })}
-        </div>
+      {
+        !this.state.cards.length
+          ? <Spinner />
+          : this.renderMixes()
+      }
+        
+
       </div>
     );
   }
